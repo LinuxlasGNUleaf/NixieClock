f88a3ff2b4313961a4cb348091e1024b399aa8be
LinuxlasGNUleaf <jakob.l.weiss@gmx.de>
2024-07-13 17:34:51 +0200

    implement less frequent rtc sync, periodic digit rotation

--- include/main.h
+++ include/main.h
@@ -3,12 +3,18 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <DS1307RTC.h>
-//#include <RTClib.h>
 
-#define AUTO_SYNC_TICKS 10
+#define SYNC_INTERVAL_MIN 10
+
+
+#define SECOND_MILLIS (1000UL)
+#define MINUTE_MILLIS (60UL*SECOND_MILLIS)
+#define HOUR_MILLIS (60UL*MINUTE_MILLIS)
+#define DAY_MILLIS (24UL*HOUR_MILLIS)
 
 
 void sendDigits(PCF8575 expander, byte low_val, byte high_val);
 void rotateAllDigits();
 void disableAllDigits();
 void displayCurrentTime();
+void syncTime();
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,14 @@ PCF8575 hourPCF(0x24);
 PCF8575 minPCF(0x22);
 PCF8575 secPCF(0x20);
 
-unsigned long last_millis;
-unsigned long current_millis;
+unsigned long last_millis = 0;
+unsigned long current_millis = 0;
+unsigned long last_sync_millis = 0;
+
+byte last_hours = 0;
+long millis_offset = 0;
+
+bool cycle_flag = false;
 
 TimeElements tm;
 
@@ -17,14 +23,12 @@ void setup()
 
   // check RTC connection
   Serial.print("Connecting to RTC...");
-  delay(500);
-  if (RTC.read(tm))
+  while (!RTC.read(tm))
   {
-    Serial.println("done.");
+    Serial.print('.');
+    delay(200);
   }
-  {
-    Serial.println("failed.\nPlease check your wiring.");
-  }
+  Serial.println("done.");
 
   // checking whether all PCFs are connected
   Serial.println("Connecting to PCFs...");
@@ -35,18 +39,11 @@ void setup()
   if (!secPCF.isConnected())
     Serial.println("Seconds PCF is NOT connected!");
 
-  // digit rotation test
-  Serial.print("Rotating all digits...");
-  hourPCF.selectNone();
-  minPCF.selectNone();
-  secPCF.selectNone();
-  delay(500);
-  rotateAllDigits();
-  Serial.println("done.");
   disableAllDigits();
+  delay(500);
 
   // read RTC and prepare variables
-  RTC.read(tm);
+  syncTime();
   last_millis = 0;
 }
 
@@ -55,8 +52,25 @@ void loop()
   current_millis = millis();
   if (last_millis + 1000 <= current_millis)
   {
-    last_millis = current_millis;
+    if (tm.Hour != last_hours)
+    {
+      rotateAllDigits();
+      last_hours = tm.Hour;
+    }
+    last_millis = millis();
     displayCurrentTime();
+  }
+
+  if (last_sync_millis + SYNC_INTERVAL_MIN * MINUTE_MILLIS <= current_millis)
+  {
+    syncTime();
+    last_sync_millis = millis();
+  }
+
+  if (cycle_flag)
+  {
+    rotateAllDigits();
+    cycle_flag = false;
   }
 }
 
@@ -69,13 +83,18 @@ void sendDigits(PCF8575 expander, byte low_val, byte high_val)
 
 void rotateAllDigits()
 {
-  for (byte i = 0; i <= 9; i++)
+  Serial.print("Rotating all digits...");
+  for (int j = 0; j < 3; j++)
   {
-    sendDigits(hourPCF, i, i);
-    sendDigits(minPCF, i, i);
-    sendDigits(secPCF, i, i);
-    delay(500);
+    for (byte i = 0; i <= 9; i++)
+    {
+      sendDigits(hourPCF, i, i);
+      sendDigits(minPCF, i, i);
+      sendDigits(secPCF, i, i);
+      delay(200);
+    }
   }
+  Serial.println("done.");
 }
 
 void disableAllDigits()
@@ -87,11 +106,29 @@ void disableAllDigits()
 
 void displayCurrentTime()
 {
-  RTC.read(tm);
-  int hours = tm.Hour;
-  int mins = tm.Minute;
-  int secs = tm.Second;
+  unsigned long tod_millis = (millis() + millis_offset) % DAY_MILLIS;
+
+  int hours = tod_millis / HOUR_MILLIS;
+  if (hours != last_hours)
+  {
+    last_hours = hours;
+    cycle_flag = true;
+  }
+  int mins = (tod_millis - (hours * HOUR_MILLIS)) / MINUTE_MILLIS;
+  int secs = (tod_millis % MINUTE_MILLIS) / SECOND_MILLIS;
   sendDigits(hourPCF, hours % 10, hours / 10);
   sendDigits(minPCF, mins % 10, mins / 10);
   sendDigits(secPCF, secs % 10, secs / 10);
+}
+
+void syncTime()
+{
+  TimeElements tm;
+  RTC.read(tm);
+
+  // calculate offset from millis()
+  millis_offset = (tm.Hour * HOUR_MILLIS + tm.Minute * MINUTE_MILLIS + tm.Second * SECOND_MILLIS) - millis();
+  Serial.println((tm.Hour * HOUR_MILLIS + tm.Minute * MINUTE_MILLIS + tm.Second * SECOND_MILLIS));
+  Serial.println(millis());
+  Serial.println(millis_offset);
 }
